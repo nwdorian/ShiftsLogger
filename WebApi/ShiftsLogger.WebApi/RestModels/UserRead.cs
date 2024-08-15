0b1582390a2304915a71717be71c7d086da6f4ff
nwdorian <neuwirthdorian@gmail.com>
2024-08-15 04:20:40 +0200

    fix: remove Id from UserCreate rest model

--- WebApi/ShiftsLogger.WebApi/RestModels/UserRead.cs
+++ WebApi/ShiftsLogger.WebApi/RestModels/UserRead.cs
@@ -3,6 +3,7 @@
 public class UserRead
 {
     public Guid Id { get; set; }
+    public Guid ShiftId { get; set; }
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public string? Email { get; set; }
