10a57e801d0da0040fddcbe70c33c968e3af769b
nwdorian <neuwirthdorian@gmail.com>
2024-11-14 02:34:58 +0100

    pluralize controller nouns

--- WebApi/ShiftsLogger.WebApi/Controllers/UserController.cs
+++ WebApi/ShiftsLogger.WebApi/Controllers/UserController.cs
@@ -8,12 +8,12 @@ namespace ShiftsLogger.WebApi.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class UserController : ControllerBase
+public class UsersController : ControllerBase
 {
     private readonly IUserService _userService;
     private readonly IMapper _mapper;
 
-    public UserController(IUserService userService, IMapper mapper)
+    public UsersController(IUserService userService, IMapper mapper)
     {
         _userService = userService;
         _mapper = mapper;
