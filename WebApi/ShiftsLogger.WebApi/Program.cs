afc77794f101c39fae18a9a5f42c03e0e2da62a3
nwdorian <neuwirthdorian@gmail.com>
2025-05-31 22:08:59 +0200

    replace swagger with openapi

--- WebApi/ShiftsLogger.WebApi/Program.cs
+++ WebApi/ShiftsLogger.WebApi/Program.cs
@@ -16,17 +16,15 @@ builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>
 });
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+	app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
